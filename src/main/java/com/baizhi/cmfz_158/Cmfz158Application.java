e36bbe4b7be450e7c5f7f969c11b22476fcb1f10
ybei <1693255115@qq.com>
2019-08-09 22:38:31 +0800

    第一次创建cmfz_158

---
+++ src/main/java/com/baizhi/cmfz_158/Cmfz158Application.java
@@ -0,0 +1,13 @@
+package com.baizhi.cmfz_158;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cmfz158Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cmfz158Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/baizhi/cmfz_158/Cmfz158ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.baizhi.cmfz_158;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Cmfz158ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
